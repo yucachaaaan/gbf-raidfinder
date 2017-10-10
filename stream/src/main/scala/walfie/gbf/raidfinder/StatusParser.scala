1fb9746e7efcdd7bc8d89e8826f7515e64e57e18
Laforet <amadeus7806@gmail.com>
2017-10-11 00:11:37 +1300

    Clean up to last fix

--- stream/src/main/scala/walfie/gbf/raidfinder/StatusParser.scala
+++ stream/src/main/scala/walfie/gbf/raidfinder/StatusParser.scala
@@ -6,6 +6,7 @@ import scala.util.Try
 
 object StatusParser {
   /** Regexes to match raid request tweets */
+  val RaidRegexJapanese = "((?s).*)([0-9A-F]+) :参戦ID\n参加者募集！\n?(.+)\n?(.*)".r
   val RaidRegexEnglish = "((?s).*)([0-9A-F]+) :Battle ID\nI need backup!\n?(.+)\n?(.*)".r
 
   /**
