df577496cfb70c1e49c1b170435c4b90c2246e5e
Laforet <amadeus7806@gmail.com>
2017-10-11 01:23:26 +1300

    refine regex terms

--- stream/src/main/scala/walfie/gbf/raidfinder/StatusParser.scala
+++ stream/src/main/scala/walfie/gbf/raidfinder/StatusParser.scala
@@ -6,8 +6,8 @@ import scala.util.Try
 
 object StatusParser {
   /** Regexes to match raid request tweets */
-  val RaidRegexJapanese = "((?s).*)([0-9A-F]{8}+) :参戦ID\n参加者募集！\n?(.+)\n?(.*)".r
-  val RaidRegexEnglish = "((?s).*)([0-9A-F]{8}+) :Battle ID\nI need backup!\n?(.+)\n?(.*)".r
+  val RaidRegexJapanese = "((?s).*)([0-9A-F]{8}) :参戦ID\n参加者募集！\n(.+)\n?(.*)".r
+  val RaidRegexEnglish = "((?s).*)([0-9A-F]{8}) :Battle ID\nI need backup!\n(.+)\n?(.*)".r
 
   /**
     * Regex to get boss level from full name
